94b9352a8b713251f48b145c90883b229b5f039f
Ziya <ziyacetinkaya@outlook.com>
2021-02-08 22:30:22 +0300

    Add project files.

---
+++ simplehouse/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace simplehouse.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("~/")]
+        public ActionResult Index()
+        {
+            ViewBag.home = "active";
+            ViewBag.about = "";
+            ViewBag.contact = "";
+            return View();
+        }
+
+        [Route("about")]
+        public ActionResult About()
+        {
+            ViewBag.home = "";
+            ViewBag.about = "active";
+            ViewBag.contact = "";
+            return View();
+        }
+
+        [Route("contact")]
+        public ActionResult Contact()
+        {
+            ViewBag.home = "";
+            ViewBag.about = "";
+            ViewBag.contact = "active";
+            return View();
+        }
+    }
+}
---
+++ simplehouse/Controllers/PartialsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace simplehouse.Controllers
+{
+    public class PartialsController : Controller
+    {
+        public ActionResult _Menu()
+        {
+            return PartialView();
+        }
+
+        public ActionResult _OurTeam()
+        {
+            return PartialView();
+        }
+        public ActionResult _ContactForm()
+        {
+            return PartialView();
+        }
+        public ActionResult _ContactInfo()
+        {
+            return PartialView();
+        }
+
+        public ActionResult _Faqs()
+        {
+            return PartialView();
+        }
+    }
+}
